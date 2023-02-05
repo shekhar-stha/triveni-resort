4f6d1d4feace54fd94730e0bc1f76af79014b5e0
Shekhar <shekharr.shresthaa@gmail.com>
2023-02-05 23:24:42 +0545

    feat: complete website

--- js/index.js
+++ js/index.js
@@ -88,14 +88,14 @@ $('.room-images').slick({
         {
             breakpoint: 992,
             settings: {
-                slidesToShow: 3,
+                slidesToShow: 1,
                 slidesToScroll: 2,
             }
         },
         {
             breakpoint: 768,
             settings: {
-                slidesToShow: 2,
+                slidesToShow: 1,
                 slidesToScroll: 2,
             }
         },
